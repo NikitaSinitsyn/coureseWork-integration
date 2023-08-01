dbbfec58601bf4e35f5fbd724581ccfc773e4db8
Nikita <nikita.sinitsyn.lipetsk@abv.bg>
2023-08-01 20:02:37 +0300

    Initial

---
+++ src/test/java/skypro/coureseworkintegration/CoureseWorkIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.coureseworkintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoureseWorkIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
